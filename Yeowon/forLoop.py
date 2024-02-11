1f8f2bb959115f885f96baf31e193affc3d4aebd
yeowonton <yeowonyoon0109@gmail.com>
2024-02-10 20:22:26 -0800

    forLoopClass

---
+++ Yeowon/forLoop.py
@@ -0,0 +1,9 @@
+class Loop():
+    def __init__(self, start, end, count):
+        self.start = start
+        self.end = end
+        self.count = count
+    def forLoop(self):
+        for i in range(self.start, self.end, self.count):
+            print(i)
+    
